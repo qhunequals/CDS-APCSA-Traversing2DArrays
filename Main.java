d266810b4401db4722893124651763bbe3ffd244
Ryan James <ryanjames1729@gmail.com>
2025-01-28 15:52:44 +0000

    initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+// Radioactivity Starter      A+CR
+class Main {
+  public static void main(String[] args) {
+    
+		double[][] grid = {{45, 36, 17, 35, 4, 38},{25,32,60,43,60,1}, {50,4,65,42,2,26},{36,24,3,43,41,11}};
+
+		
+
+
+  }
+}
